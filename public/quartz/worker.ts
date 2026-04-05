bcbbf901b2e68b65efa7ef887125285fe69db325
camac <camac@camac-MacBookPro.local>
2026-04-06 00:45:12 +0800

    Add public/ build output and fix .gitignore

---
+++ public/quartz/worker.ts
@@ -0,0 +1,26 @@
+import sourceMapSupport from "source-map-support"
+sourceMapSupport.install(options)
+import cfg from "../quartz.config"
+import { Argv, BuildCtx } from "./util/ctx"
+import { FilePath, FullSlug } from "./util/path"
+import { createFileParser, createProcessor } from "./processors/parse"
+import { options } from "./util/sourcemap"
+
+// only called from worker thread
+export async function parseFiles(
+  buildId: string,
+  argv: Argv,
+  fps: FilePath[],
+  allSlugs: FullSlug[],
+) {
+  console.log('parseFiles buildId', buildId)
+  const ctx: BuildCtx = {
+    buildId,
+    cfg,
+    argv,
+    allSlugs,
+  }
+  const processor = createProcessor(ctx)
+  const parse = createFileParser(ctx, fps)
+  return parse(processor)
+}
